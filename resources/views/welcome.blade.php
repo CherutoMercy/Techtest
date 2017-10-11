e346695d613ba3ee9b2943d05fdecc6343978c87
CherutoMercy <m.thirty100@gmail.com>
2017-10-12 00:43:12 +0300

    make responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,6 @@
 @extends('layout')
-@section('content')
-<section id="main-content">
- <section class="wrapper">            
-    <div class="row visible-lg">
+@section('content')          
+    <div class="container">
       <div class="content">
           <div class="title m-b-md">
               <h1>Temper</h1>
@@ -10,6 +8,4 @@
           <div id="container" style="width:100%; height:400px;"></div>
       </div>
     </div>
- </section>
-</section>
 @endsection
